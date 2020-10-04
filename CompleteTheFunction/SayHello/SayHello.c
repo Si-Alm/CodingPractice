c94bac2ab76ee6e4e2a0ec3f5ea1d247bd5dba4e
Silas <si@Silas-MacBook-Pro.local>
2020-10-04 12:24:00 -0600

    added say hello practice

---
+++ CompleteTheFunction/SayHello/SayHello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void sayHello(*char name) {
+	//finish the say hello function
+	//reminder: function should print "Hello, <name>!"
+}
+
+int main() {
+	//Make it more interesting, prompt a user for their name and call the function with user input
+	char name[] = "World";
+	sayHello(name);
+
+	return 0;
+}
