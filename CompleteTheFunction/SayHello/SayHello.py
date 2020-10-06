0e0f053dbabde0129a207786032e3a0d03db0a43
mrtentacless <72365974+mrtentacless@users.noreply.github.com>
2020-10-05 20:13:16 -0600

    Update SayHello.py
    Well, I could use some refreshment in the world of python... After half an hour of tutorials, this should work.

--- CompleteTheFunction/SayHello/SayHello.py
+++ CompleteTheFunction/SayHello/SayHello.py
@@ -1,9 +1,13 @@
 def main():
-	#Hello world test
-	say_hello("World")
-	
-	#Make it more interesting, prompt a user for their name and call the function with user input
+#Make it more interesting, prompt a user for their name and call the function with user input
+	name = input("Enter yo name: ")
+	say_hello(name)
 
 def say_hello(name):
-	#finish the say hello function
+  	#finish the say hello function
 	#reminder: function should print "Hello, <name>!"
+
+	print("Hello, " + name + "!")
+
+
+main()
